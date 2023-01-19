bfb9b30f30e88057d965a1f58b7627da4fe1e0a9
= <=>
2023-01-19 13:04:08 +0530

    reload problem solved

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ state = {
     arr : []
   };
 change = (e)=>{
-  e.preventDefault()
+
 console.log(e.target.value);
 this.setState({[e.target.name]: e.target.value})
 
@@ -32,14 +32,14 @@ showData = () =>{
     return (
       <div>
         <h1 align='center'>EMPLOYEE FEEDBACK FORM</h1>
-        <form align='center' action='#'>
+        <form align='center'>
         <label>Name : &nbsp;&nbsp; </label>
-        <input type='text' name="Name" value={this.state.Name} onChange={this.change} required/> <br />
+        <input type='text' name="Name" value={this.state.Name} onChange={this.change} /> <br />
         <label>Department :</label>
-        <input type='text' name="Department" value={this.state.Department} onChange={this.change} required/> <br />
+        <input type='text' name="Department" value={this.state.Department} onChange={this.change} /> <br />
         <label>Rating : &nbsp;</label>
-        <input type='number' name="Rating" value={this.state.Rating} onChange= {this.change} required/> <br/>
-        <button type='submit' align='center' onClick={this.showData}>Submit</button>
+        <input type='number' name="Rating" value={this.state.Rating} onChange= {this.change} /> <br/>
+        <button type='button' align='center' onClick={this.showData}>Submit</button>
         </form>
         <div className='container'>
           {this.state.arr.map((value,index)=>{
